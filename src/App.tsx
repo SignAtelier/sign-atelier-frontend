6df623824191e0b0023eed49c907b11a2d5316ef
ChaJunWoo18 <wnsdnxla@naver.com>
2025-07-03 21:58:42 +0900

    feat: 싸인 목록, 연습 페이지 라우팅 등록

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Practice from "./pages/Practice";
+import SignatureList from "./pages/SignatureList";
 import SignatureResult from "./pages/SignatureResult";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signature/result" element={<SignatureResult />} />
+        <Route path="/signature/list" element={<SignatureList />} />
+        <Route path="/signature/practice" element={<Practice />} />
       </Routes>
     </Router>
   );
