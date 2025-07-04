69926d0537c9727dda9a356ce9b4b5dee0e2b53e
ChaJunWoo18 <wnsdnxla@naver.com>
2025-07-04 15:02:59 +0900

    refactor: 모달 border 제거

--- src/shared/components/Modal.tsx
+++ src/shared/components/Modal.tsx
@@ -5,7 +5,7 @@ const Modal = ({ children, onClose }: ModalProps) => {
   return (
     <div className="fixed inset-0 z-50 flex justify-center items-center">
       <div className="absolute inset-0 bg-black opacity-40" />
-      <div className="relative py-4 px-6 bg-white rounded-md space-y-4 border z-10">
+      <div className="relative py-4 px-6 bg-white rounded-md space-y-4 z-10">
         <div className="flex justify-end">
           <IoCloseOutline
             className="text-2xl cursor-pointer"
