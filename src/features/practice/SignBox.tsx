16ea82c134b4a98d3edf410b481495078e26b3c7
ChaJunWoo18 <wnsdnxla@naver.com>
2025-07-04 15:11:20 +0900

    feat: 싸인 박스 컴포넌트 구현

---
+++ src/features/practice/SignBox.tsx
@@ -0,0 +1,40 @@
+import ToggleSwitch from "./ToggleSwitch";
+import type { SignBoxProps } from "./types";
+
+const SignBox = ({
+  title,
+  imageSrc,
+  showImage,
+  showOutline,
+  onToggleImage,
+  onToggleOutline,
+}: SignBoxProps) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-base font-semibold text-gray-700">{title}</p>
+
+      <div className="p-4 w-[256px] h-[128px] border border-gray-400 rounded-xl overflow-hidden flex items-center justify-center bg-white">
+        {showImage ? (
+          <img src={imageSrc} alt="sign" />
+        ) : (
+          <span className="text-sm text-gray-400">이미지 숨김</span>
+        )}
+      </div>
+
+      <div className="flex gap-4">
+        <ToggleSwitch
+          label="이미지 보기"
+          checked={showImage}
+          onToggle={onToggleImage}
+        />
+        <ToggleSwitch
+          label="윤곽선 보기"
+          checked={showOutline}
+          onToggle={onToggleOutline}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SignBox;
