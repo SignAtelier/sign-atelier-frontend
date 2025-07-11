4a83456a11ca7bba4a96c5057e69f2ad9655114f
ChaJunWoo18 <wnsdnxla@naver.com>
2025-07-11 15:46:21 +0900

    feat: 싸인 목록 페이지 로딩 컴포넌트 적용

--- src/pages/SignatureList.tsx
+++ src/pages/SignatureList.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getSigns } from "../apis/signs";
 import SignCard from "../features/sign/SignCard";
 import Header from "../shared/components/Header";
+import Loading from "../shared/components/Loading";
 
 interface Sign {
   id: string;
@@ -19,23 +20,22 @@ interface newSign {
 }
 
 const SignatureList = () => {
-  const [signs, setSigns] = useState<Sign[]>();
+  const [signs, setSigns] = useState<Sign[] | null>(null);
 
   const handleSignUpdate = (newSign: newSign) => {
-    setSigns((prev) =>
-      prev?.map((sign) => {
-        if (sign.id === newSign.id) {
-          return { ...sign, name: newSign.name };
-        }
+    setSigns((prev) => {
+      if (!prev) return null;
 
-        return sign;
-      })
-    );
+      return prev.map((sign) =>
+        sign.id === newSign.id ? { ...sign, name: newSign.name } : sign
+      );
+    });
   };
 
   useEffect(() => {
     (async () => {
-      setSigns(await getSigns());
+      const result = await getSigns();
+      setSigns(result);
     })();
   }, []);
 
@@ -48,20 +48,18 @@ const SignatureList = () => {
           <p className="text-2xl p-2 font-black">내 사인 목록</p>
         </div>
 
-        {signs ? (
-          <>
-            <div className="grid grid-cols-5 grid-rows-3 gap-5">
-              {signs.map((sign) => (
-                <SignCard
-                  key={sign.id}
-                  sign={sign}
-                  onUpdate={handleSignUpdate}
-                />
-              ))}
-            </div>
-          </>
+        {signs === null ? (
+          <Loading>싸인을 가져오고 있어요</Loading>
+        ) : signs.length === 0 ? (
+          <div className="w-full h-[300px] flex items-center justify-center text-gray-400 text-xl">
+            아직 생성한 싸인이 없어요.
+          </div>
         ) : (
-          <></>
+          <div className="grid grid-cols-5 gap-5">
+            {signs.map((sign) => (
+              <SignCard key={sign.id} sign={sign} onUpdate={handleSignUpdate} />
+            ))}
+          </div>
         )}
       </div>
     </div>
