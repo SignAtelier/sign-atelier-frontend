f5b8bea600d8dd53d9133287ec0ac5a350b8f1a5
ChaJunWoo18 <wnsdnxla@naver.com>
2025-07-11 15:47:27 +0900

    feat: 로고 클릭 시 메인 페이지 이동

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "../../assets/signLogo.png";
 import LoginModal from "../../features/auth/LoginModal";
 import Profile from "../../features/auth/Profile";
@@ -14,13 +15,18 @@ const headerClass = `
 const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-
   const { userInfo } = useUserStore();
+  const navigate = useNavigate();
 
   return (
     <header className={headerClass}>
       {isModalOpen && <LoginModal onClose={() => setIsModalOpen(false)} />}
-      <img src={logo} alt="싸인공방" className="w-32 h-auto" />
+      <img
+        src={logo}
+        alt="싸인공방"
+        className="w-32 h-auto cursor-pointer"
+        onClick={() => navigate("/")}
+      />
       {userInfo ? (
         <div className="relative">
           <Profile
